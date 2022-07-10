258596f6d1d4acab77d29f353ecbf616fa8b23f9
inu <inu@voxteneo.com>
2022-07-10 14:05:03 +0700

    init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.mydramalist.com', 'lh3.googleusercontent.com']
+  }
 }
 
 module.exports = nextConfig
